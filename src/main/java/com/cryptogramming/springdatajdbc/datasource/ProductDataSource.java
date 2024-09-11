083aaea81bca56e656743a33e967b54b550d329e
Emmanuel <emmanueleedm@gmail.com>
2024-09-11 17:17:09 -0300

    remove AppConfig class

--- src/main/java/com/cryptogramming/springdatajdbc/datasource/ProductDataSource.java
+++ src/main/java/com/cryptogramming/springdatajdbc/datasource/ProductDataSource.java
@@ -14,31 +14,27 @@ public class ProductDataSource {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    // Insertar un nuevo producto
     public void addProduct(Product product) {
         String sql = "INSERT INTO food.product (image_path, title, description, distributor, release_date) VALUES (?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, product.getImagePath(), product.getTitle(), product.getDescription(), product.getDistributor(), product.getReleaseDate());
     }
 
-    // Obtener un producto por ID
     public Product getProductById(int id) {
         String sql = "SELECT * FROM food.product WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(Product.class));
     }
 
-    // Actualizar un producto
     public void updateProduct(Product product) {
         String sql = "UPDATE food.product SET imagePath = ?, title = ?, description = ?, distributor = ?, releaseDate = ? WHERE id = ?";
         jdbcTemplate.update(sql, product.getImagePath(), product.getTitle(), product.getDescription(), product.getDistributor(), product.getReleaseDate(), product.getId());
     }
 
-    // Eliminar un producto por ID
     public void deleteProduct(int id) {
         String sql = "DELETE FROM food.product WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
-    // Obtener todos los productos
+
     public List<Product> getAllProducts() {
         String sql = "SELECT * FROM food.product";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Product.class));
